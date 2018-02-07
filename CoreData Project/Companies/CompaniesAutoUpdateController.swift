1f512722d194a8e4a81922a1951e1475da03705c
Nuno Pereira <nuno-f-pereira@telecom.pt>
2018-02-07 01:08:36 +0000

    Project now working with NSFetchedResultsController. Now is possible to insert, move, update, and delete in the correct tableView position, with no problem.

---
+++ CoreData Project/Companies/CompaniesAutoUpdateController.swift
@@ -0,0 +1,167 @@
+//
+//  CompaniesAutoUpdateController.swift
+//  CoreData Project
+//
+//  Created by Nuno Pereira on 04/02/2018.
+//  Copyright © 2018 Nuno Pereira. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CompaniesAutoUpdateController: UITableViewController, NSFetchedResultsControllerDelegate {
+    
+    let cellId = "cellId"
+    
+    lazy var fetchedResultsController: NSFetchedResultsController<Company> = {
+        let context = CoreDataManager.shared.persistentContainer.viewContext;
+        let request: NSFetchRequest<Company> = Company.fetchRequest()
+        
+        //Definicao da key name significa que e com base nesta que ira ser feita a ordenacao
+        request.sortDescriptors = [
+            NSSortDescriptor(key: "name", ascending: true)
+        ]
+        
+        let frc = NSFetchedResultsController(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: "name", cacheName: nil)
+        
+        frc.delegate = self
+        
+        do {
+            try frc.performFetch()
+        }
+        catch let err {
+            print(err)
+        }
+        return frc
+    }()
+    
+    
+    //E chamado antes do save
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .fade)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .fade)
+        case .move:
+            break
+        case .update:
+            break
+        }
+    }
+    
+    //E chamado sempre que ha um alteracao no CoreData seja, insercao, remocao, alteracao, movimentacao
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            tableView.insertRows(at: [newIndexPath!], with: .fade)
+        case .delete:
+            tableView.deleteRows(at: [indexPath!], with: .fade)
+        case .update:
+            tableView.reloadRows(at: [indexPath!], with: .fade)
+        case .move:
+            tableView.moveRow(at: indexPath!, to: newIndexPath!)
+        }
+    }
+    
+    //E chamado depois do save
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, sectionIndexTitleForSectionName sectionName: String) -> String? {
+        return sectionName
+    }
+    
+    @objc func handleAdd() {
+        
+        let context = CoreDataManager.shared.persistentContainer.viewContext
+        let company = Company(context: context)
+        
+        company.name = "BMW"
+        
+        do {
+            try context.save()
+        }
+        catch let err {
+            print("Failed to sabe: ", err)
+        }
+    }
+    
+    @objc func handleDelete() {
+        
+        let request: NSFetchRequest<Company> = Company.fetchRequest()
+        request.predicate = NSPredicate(format: "name CONTAINS %@", "B")
+        
+        let context = CoreDataManager.shared.persistentContainer.viewContext
+        
+        do {
+            let companiesWithB = try context.fetch(request)
+            
+            companiesWithB.forEach({ (company) in
+                context.delete(company)
+            })
+            
+            do {
+                try context.save()
+            }
+            catch let err {
+                print("Failed to delete companies with predicate: ", err)
+            }
+        }
+        catch let err {
+            print("Failed fetching companies with predicate: ", err)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .darkBlue
+        
+        navigationItem.leftBarButtonItems = [
+            UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(handleAdd)),
+            UIBarButtonItem(title: "Delete", style: .plain, target: self, action: #selector(handleDelete))
+        ]
+        
+        navigationItem.title = "Company Auto Updates!"
+        
+        tableView.register(CompanyCell.self, forCellReuseIdentifier: cellId)
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = IndentedLabel()
+        label.backgroundColor = .lightBlue
+        label.text = fetchedResultsController.sectionIndexTitles[section]
+        return label
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //A propriedade retorna um array das seccoes e em cada posicao o numero de objectos
+        return fetchedResultsController.sections![section].numberOfObjects
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! CompanyCell
+        
+        let company = fetchedResultsController.object(at: indexPath)
+        
+        cell.company = company
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+}
--- CoreData Project/Companies/CompaniesController.swift
+++ CoreData Project/Companies/CompaniesController.swift
@@ -15,6 +15,122 @@ class CompaniesController: UITableViewController {
 
     var companies = [Company]()
     
+    @objc func doWork() {
+        print("Trying to do work...")
+
+        CoreDataManager.shared.persistentContainer.performBackgroundTask { (backgroundContext) in
+            (0...10).forEach { (value) in
+                print(value)
+                let company = Company(context: backgroundContext)
+                company.name = String(value)
+            }
+            do {
+                try backgroundContext.save()
+                
+                DispatchQueue.main.async {
+                    self.companies = CoreDataManager.shared.fetchCompanies()
+                    self.tableView.reloadData()
+                }
+            }
+            catch let err {
+                print("Failed to save: ", err)
+            }
+        }
+    }
+    
+    @objc func doUpdates() {
+        print("Trying to update companies on background context...")
+        
+        CoreDataManager.shared.persistentContainer.performBackgroundTask { (backgroundContext) in
+            
+            //Tem que se especificar o tipo NSFetchRequest e o objecto senao da erro que se trata de um fetchRequest muito generico
+            let request: NSFetchRequest<Company> = Company.fetchRequest()
+            
+            do {
+                //Nao se pode utilizar as self.companies pois estas foram adquiridas noutro contexto neste caso na main thread e por isso tem que ser feito outro fetch request
+                let companies = try backgroundContext.fetch(request)
+                
+                companies.forEach({ (company) in
+                    print(company.name ?? "")
+                    company.name = "C: \(company.name ?? "")"
+                    
+                    do {
+                        try backgroundContext.save()
+                        
+                        DispatchQueue.main.async {
+                            
+                            //reset will forget all of the objects you've fetch before
+                            CoreDataManager.shared.persistentContainer.viewContext.reset()
+                            self.companies = CoreDataManager.shared.fetchCompanies()
+                            self.tableView.reloadData()
+                        }
+                    }
+                    catch let err {
+                        print("Failed to save companies on background: ", err)
+                    }
+                })
+            }
+            catch let err {
+                print("Failed to fetch companies on background: ", err)
+            }
+        }
+    }
+    
+    @objc func doNestedUpdates() {
+        print("trying to perform nested updades")
+        
+        DispatchQueue.global(qos: .background).async {
+            //Going to try to perform our updates
+            
+            //first construct a custom MOC
+            //Utiliza-se o private queue por estar a ser utilizado um backgroundContext
+            let privateContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+            
+            //Definir o parent
+            privateContext.parent = CoreDataManager.shared.persistentContainer.viewContext
+            
+            //Executar os updates no privateContext
+            let request: NSFetchRequest<Company> = Company.fetchRequest()
+            //Reduzir o limite de objectos retornado pelo request
+            request.fetchLimit = 1
+            
+            do {
+                let companies = try privateContext.fetch(request)
+                
+                companies.forEach({ (company) in
+                    print(company.name ?? "")
+                    company.name = "D: \(company.name ?? "")"
+                })
+                
+                do {
+                    try privateContext.save()
+                    
+                    DispatchQueue.main.async {
+                        
+                        do {
+                            let context = CoreDataManager.shared.persistentContainer.viewContext
+                            //Validacao necessaria para evitar que o CoreData tenha trabalho desnecessario
+                            if context.hasChanges {
+                                try context.save()
+                            }
+                        }
+                        catch let finalSaveErr {
+                            print("Failed to save main context: ", finalSaveErr)
+                        }
+                        self.tableView.reloadData()
+                    }
+                }
+                catch let saveErr {
+                    print("Failed to save on private context: ", saveErr)
+                }
+            }
+            catch let err {
+                print("Failed to fetch on private context: ", err)
+            }
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,7 +140,14 @@ class CompaniesController: UITableViewController {
         
         navigationItem.title = "Companies"
 //        navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddCompany))
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Reset", style: .plain, target: self, action: #selector(handleReset))
+//        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Reset", style: .plain, target: self, action: #selector(handleReset))
+        
+        
+        navigationItem.leftBarButtonItems = [
+            UIBarButtonItem(title: "Reset", style: .plain, target: self, action: #selector(handleReset)),
+            UIBarButtonItem(title: "Nested Updates", style: .plain, target: self, action: #selector(doNestedUpdates))
+        ]
+        
         
         setupPlusButtonInNavBar(selector: #selector(handleAddCompany))
         
